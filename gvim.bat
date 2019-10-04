7751be7d159994842c1dd09b5f91612b42e737f5
Jay Ache <jayache80@gmail.com>
2019-10-04 14:42:58 -0700

    Use gvim 8.1

--- gvim.bat
+++ gvim.bat
@@ -2,8 +2,8 @@
 rem -- Run Vim --
 
 setlocal
-set VIM_EXE_DIR=C:\Program Files (x86)\Vim\vim80
-if exist "%VIM%\vim80\gvim.exe" set VIM_EXE_DIR=%VIM%\vim80
+set VIM_EXE_DIR=C:\Program Files (x86)\Vim\vim81
+if exist "%VIM%\vim81\gvim.exe" set VIM_EXE_DIR=%VIM%\vim81
 if exist "%VIMRUNTIME%\gvim.exe" set VIM_EXE_DIR=%VIMRUNTIME%
 
 if exist "%VIM_EXE_DIR%\gvim.exe" goto havevim
