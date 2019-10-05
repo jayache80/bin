e3ac2f853b4fb1fadcfe3ae5d48acdfd5ddfcd26
Jay Ache <jayache80@gmail.com>
2019-10-04 20:36:11 -0700

    Fix total elapsed time

--- space.ps1
+++ space.ps1
@@ -3,17 +3,19 @@ $timer = [system.diagnostics.stopwatch]::StartNew()
 $totalsize = [long]0
 Get-ChildItem -File -Recurse -Force -ErrorAction SilentlyContinue | % {
     $totalsize += $_.Length;
-    if ($timer.Elapsed.TotalSeconds -gt 2){
+    if ($timer.Elapsed.TotalSeconds -gt 2) {
         Write-Host "Current file:"
         Write-Host $_.FullName
+        $totaltime = "{0:n2}" -f $totaltimer.Elapsed.TotalSeconds
+        Write-Host "Elapsed time: ${totaltime} seconds"
         $sizegb = "{0:n3}" -f ($totalsize/1Gb)
-        Write-Host "size so far is ${sizegb}GB"
+        Write-Host "Size so far: ${sizegb}GB"
         $timer.Reset()
         $timer.Start()
     }
 }
 Write-Host "Complete!"
-$totaltime = "{0:n2}" -f $timer.Elapsed.TotalSeconds
-Write-Host "Elapsed time: ${totaltime} seconds" 
+$totaltime = "{0:n2}" -f $totaltimer.Elapsed.TotalSeconds
+Write-Host "Total elapsed time: ${totaltime} seconds"
 $sizegb = "{0:n3}" -f ($totalsize/1Gb)
 Write-Host -NoNewLine "total size is ${sizegb}GB"
